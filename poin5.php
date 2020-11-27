f3bc01a4be30401e71c842eaca6bb5cb9cf18f4c
Bagas Ghufron Alfaiz <bagas.al@students.amikom.ac.id>
2020-11-27 20:49:54 +0700

    poin5

---
+++ poin5.php
@@ -0,0 +1,20 @@
+<?php
+$input = 'DFHKNQ';
+$split = str_split($input);
+$panjang = count($split);
+
+echo "Input : " . $input;
+echo "<br>";
+echo "Output : ";
+
+for($i=1;$i<= $panjang;$i++){
+    if ($i % 2 == 0) {
+        $char = ord($split[$i - 1]) - $i;
+        echo chr($char);
+
+    } else {
+        $char = ord($split[$i - 1]) + $i;
+        echo chr($char);
+    } 
+}
+?>
